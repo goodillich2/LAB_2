03c560dedb706b9f487d720bf4fae08cf6d0265d
goodillich2 <goodillich3@gmail.com>
2021-10-27 22:27:00 +0300

    all project

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,172 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    public static void main(String[] args) {
+        StringCalculator p = new StringCalculator();
+        p.Add("-1,-2,-3,4444,dfdfd");
+
+
+
+    }
+
+    public int Add(String numbers)  {
+        if(numbers.equals("")){
+            return 0;
+        }
+        else if(consist_negative_numbers(numbers)) {
+            try{
+                throw new IOException();
+            }
+            catch (IOException e){
+                System.out.print("negatives not allowed: " );
+                for(String str: print_negative_numbers(numbers)){
+                    if(str!=null){
+                        System.out.print(str+" ");
+                    }
+                }
+
+                return -1;
+            }
+        }
+        else if(numbers.length() == 1){
+            return Integer.parseInt(numbers);
+        }
+        else{
+            String delimiter = "";
+            String delimiter2 = "";
+            if(numbers.contains("//")){
+                if(!multipleDelimiter(numbers)) {
+                    char[] t = numbers.toCharArray();
+                    for (int i = 2; i < t.length; i++) {
+                        if (t[i] != '\n') {
+                            if(t[i] == '[' || t[i] == ']')continue;
+                            else delimiter += String.valueOf(t[i]);
+                        }
+                        else break;
+                    }
+                    numbers = numbers.substring(5 + delimiter.length());
+                }
+                else{
+                    char[] t1 = numbers.toCharArray();
+                    int sum1 = 0;
+                    for (int i = 2; i < t1.length; i++){
+                        if(t1[i]!='\n') {
+                            if ( t1[i] == ']'){
+                                sum1++;
+                                continue;
+                            }
+                            if ( t1[i] =='['){
+                                continue;
+                            }
+                            if ( t1[i]!='[' && t1[i]!=']' && sum1 == 0){
+                                delimiter+=t1[i];
+                            }
+                            if ( t1[i]!='[' && t1[i]!=']' && sum1 == 1){
+                                delimiter2+=t1[i];
+                            }
+
+                        }
+                        else break;
+                    }
+                    numbers = numbers.substring(7 + delimiter.length()+delimiter2.length());
+                }
+            }
+            else  delimiter = ",";
+
+            if(delimiter2.equals("")) {
+                String[] str = numbers.replace("\n", delimiter).split(Pattern.quote(delimiter));
+                int sum = 0;
+                for (String number : str) {
+                    if(isNumeric(number)) sum += ignore_number_bigger_than_1000(Integer.parseInt(number));
+                    else continue;
+                }
+                return sum;
+            }
+            else{
+                Formatter f = new Formatter();
+                f.format("[%s%s] ?", delimiter, delimiter2);
+                String[] str1 = numbers.split(String.valueOf(f));
+                int sum = 0;
+                for (String number : str1) {
+                    if(isNumeric(number)) sum += ignore_number_bigger_than_1000(Integer.parseInt(number));
+                    else continue;
+                }
+                return sum;
+            }
+        }
+        /*else {
+            int sum = 0;
+            String numberOnly= numbers.replaceAll("[^0-9]", "");
+            char[] res  = numberOnly.toCharArray();
+            for(char number: res){
+                sum+=  Integer.parseInt(String.valueOf(number));
+            }
+            return sum;
+        }*/
+
+    }
+
+    public  static boolean consist_negative_numbers(String string){
+        char[] ar = string.toCharArray();
+        for(int i = 0; i<ar.length; i++){
+            if(ar[i] == '-' && Character.isDigit(ar[i+1]))
+                return true;
+        }
+        return false;
+    }
+
+    public  static String[] print_negative_numbers(String string){
+       int j =0;
+        char[] ar = string.toCharArray();
+        String[] negatives = new String[10];
+        for(int i = 0; i<ar.length; i++){
+            if(ar[i] == '-' )
+            {
+                negatives[j] = "-";
+               while (Character.isDigit(ar[i+1]) ){
+                   negatives[j] = negatives[j]+ar[i + 1];
+                   if( i+1== ar.length-1){
+                       break;
+                   }
+                   else i++;
+               }
+               j++;
+            }
+        }
+        return negatives;
+    }
+
+    public static int ignore_number_bigger_than_1000(int number){
+        if(number>1000){
+            return 0;
+        }
+        else return number;
+    }
+
+    public static boolean multipleDelimiter(String string){
+        int sum = 0;
+        char[] a = string.toCharArray();
+        for(char ch: a){
+            if (ch =='[')sum++;
+        }
+        if (sum == 1) return false;
+        else return true;
+    }
+    public static boolean isNumeric(String str)
+    {
+        try
+        {
+            double d = Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,68 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import java.io.IOException;
+
+public class FirstTest {
+    private StringCalculator p;
+    private final double EPS = 1e-9;
+
+    @Before
+    public void before(){
+         p = new StringCalculator();
+    }
+
+    @Test
+    public void TestShouldReturnZeroForEmptyString(){
+        Assert.assertEquals(0, p.Add(""));
+    }
+
+    @Test()
+    public void TestShouldReturnOneNumber(){
+        Assert.assertEquals(5,p.Add("5"));
+    }
+
+    @Test()
+    public void TestShouldAddTwoNumbers(){
+        Assert.assertEquals(30,p.Add("5,25"));
+    }
+
+    @Test()
+    public void TestShouldAddUnknownAmountOfNumbers(){
+
+        Assert.assertEquals(11,p.Add("5,2,4"));
+    }
+
+    @Test()
+    public void TestShouldAllowNewLineBetweenNumbers(){
+
+        Assert.assertEquals(6,p.Add("1\n2,3"));
+    }
+    @Test()
+    public void TestShouldAllowCustomDelimiter(){
+        Assert.assertEquals(3,p.Add("//[;]\n1;2"));
+    }
+    @Test()
+    public void TestShouldRejectNegatives(){
+        Assert.assertEquals(-1, p.Add("1,-3"));
+    }
+
+    @Test()
+    public void TestShouldIgnoreNumberBiggerThan1000(){
+        Assert.assertEquals(10, p.Add("5,1001,5"));
+    }
+    @Test
+    public void TestShouldAllowCustomDelimiterCpntainsMoreThanOneSymbol(){
+        Assert.assertEquals(6, p.Add( "//[***]\n1***2***3***"));
+    }
+
+    @Test
+    public void TestShouldAllowAllowMultipleDelimiters(){
+        Assert.assertEquals(8, p.Add( "//[+][%]\n1+6%1+"));
+    }
+
+    @Test
+    public void TestShouldAllowAllowMultipleDelimitersWithLengthLongerThanOneChar(){
+        Assert.assertEquals(6, p.Add(  "//[**][%%]\n1**2%%3"));
+    }
+}
